a4841304975561c7e515ac2f05bd9615bef29fe6
Christopher Ferreira Silva <christopher.ferreira.silva@outlook.com>
2021-03-19 17:01:53 -0300

    Validando os error para entrar na partida

--- TARS/Telas/EntrarPartida.cs
+++ TARS/Telas/EntrarPartida.cs
@@ -15,6 +15,7 @@ namespace TARS.Telas
     public partial class EntrarPartida : Form
     {
         public int idpartida { get; set; }
+
         public EntrarPartida(int idpartida)
         {
             InitializeComponent();
@@ -31,15 +32,25 @@ namespace TARS.Telas
         {
             string nomejogador = txt_nomejogador.Text;
             string senha = txt_senha.Text;
-            Jogo.EntrarPartida(idpartida, nomejogador, senha);
-            txt_nomejogador.Text = "";
-            txt_senha.Text = "";
-            MessageBox.Show("Você entrou na partida");
-            Tabuleiro tabuleiro = new Tabuleiro();
-            Menu menu = new Menu();
-            tabuleiro.Show();
-            this.Close();
-            menu.Close();
+            string validadorEntrarPartida = Jogo.EntrarPartida(idpartida, nomejogador, senha);
+
+            if (validadorEntrarPartida[0] == 'E')
+            {
+                MessageBox.Show(validadorEntrarPartida);
+                txt_nomejogador.Text = "";
+                txt_senha.Text = "";
+            }
+            else
+            {
+                txt_nomejogador.Text = "";
+                txt_senha.Text = "";
+                MessageBox.Show("Você entrou na partida");
+                Tabuleiro tabuleiro = new Tabuleiro();
+                Menu menu = new Menu();
+                this.Close();
+                tabuleiro.Show();
+            }
+
         }
 
         private void btn_cancelar_Click(object sender, EventArgs e)
